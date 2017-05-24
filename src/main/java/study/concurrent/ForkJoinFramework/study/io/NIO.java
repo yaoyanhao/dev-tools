e3280eeedc2acf2ea235cf649eb1134c2a2f32cd
vector01.yao <vector01.yao@vipshop.com>
2017-05-24 11:10:20 +0800

    【补充】NIO读写数据练习实例

--- src/main/java/study/concurrent/ForkJoinFramework/study/io/NIO.java
+++ src/main/java/study/concurrent/ForkJoinFramework/study/io/NIO.java
@@ -25,13 +25,13 @@ public class NIO {
             ByteBuffer byteBuffer=ByteBuffer.allocate(10);//分配Buffer空间
             int readTag=fileChannel.read(byteBuffer);//1.将文件数据通过Channel写入Buffer，无数据可写时为-1
             while (readTag!=-1){
-                byteBuffer.flip();//2.从写模式切换到读模式====>开始准备读咯
-                while (byteBuffer.hasRemaining()){//当缓存区中还有“余粮”，就不停读.....
-                    System.out.print(byteBuffer.get());
+                byteBuffer.flip();//2.从写模式切换到读模式====>position重置为0，limit设置为之前写模式的position值
+                while (byteBuffer.hasRemaining()){//当缓存区中还有“余粮”，就不停读.....=====>position<capicity-1
+                    System.out.print(byteBuffer.get());//=====>position++
                 }
-                byteBuffer.rewind();//buffer倒回=====>position重置为0，实现Buffer的重复读取
+                byteBuffer.rewind();//buffer倒回=====>position重置为0，limit保持不变，重读buffer中的数据
                 outChannel.write(byteBuffer);//读取buffer中的数据，写入输出channal中
-                byteBuffer.clear();//清空Buffer
+                byteBuffer.clear();//清空Buffer===>注：Buffer中数据并未真正清除，只是position被设置为0，limit被置为了capicity值
                 readTag=fileChannel.read(byteBuffer);//再次写入Buffer
             }
         } catch (IOException e) {
